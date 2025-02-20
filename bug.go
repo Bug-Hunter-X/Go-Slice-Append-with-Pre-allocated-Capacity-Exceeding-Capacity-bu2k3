ba73577b127ba6b68f506a958ab092ba108c31fe
BugHunter-X <askzenai@gmail.com>
2025-02-20 21:40:06 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,7 @@
+func main() {
+  x := make([]int, 0, 10)
+  x = append(x, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  fmt.Println(x)
+  x = append(x, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)
+  fmt.Println(x)
+}
